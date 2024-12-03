6d78e84cfd8bdb8f66883c1d14e24d0e2ac3976f
Christian <jedchrisi09@gmail.com>
2024-12-03 19:38:41 +0100

    S-HUD examples

---
+++ S-HUD.lua
@@ -0,0 +1,16 @@
+-- Client
+
+-- Normal info Notify
+exports["S-HUD"]:MakeNotifyInfo("info", "S-Service", "Hey, that's the best Notify Script", 5000)
+
+-- Announce Notify
+exports["S-HUD"]:MakeAnnounce("Hey, that's the best Notify Script", "server", "Chrisi", 5000)
+
+-- Team Notify
+exports["S-HUD"]:MakeTeamNotify("Chrisi", "Hey, that's the best Notify Script", 5000)
+
+-- Key Help Notify
+exports["S-HUD"]:MakeHelpNotify("E", "Hey, that's the best Notify Script")
+
+-- Progress Bar
+exports["S-HUD"]:MakeProgressbar("Hey, that's the best Notify Script", 5000)
